358eec757b567761714ac2b850aa6434ddfc9a1b
Trey <trey@morris2morris.com>
2024-01-28 19:33:34 -0500

    Update SpringApiApplicationTests.java

--- src/test/java/com/example/springapi/SpringApiApplicationTests.java
+++ src/test/java/com/example/springapi/SpringApiApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.springapi;
-
+import com.example.springapi.controller.UserController;
+import com.example.springapi.model.User;
+import com.example.springapi.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class SpringApiApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    private UserService service;
+    private UserController controller;
+    private MockMvc mockMvc;
+    @Test
+    public void testShouldReturn404NotFound() throws Exception
+    {
+
+
     }
 
 }
